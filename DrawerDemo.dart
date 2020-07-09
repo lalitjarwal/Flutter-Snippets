a7fa10d05f8939b71cf072b71d7f2c42f6d3b905
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-07-09 12:23:42 +0530

    Drawer navigation in a flutter app.

---
+++ DrawerDemo.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Drawer Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Widget _childBody = HomeWidget();
+  static var title = 'Home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: _childBody,
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Home'),
+              onTap: () {
+                setState(() {
+                  title = 'Home';
+                  _childBody = HomeWidget();
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                setState(() {
+                  title = 'item1';
+                  _childBody = Item1();
+                });
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                setState(() {
+                  title = 'item2';
+                  _childBody = Item2();
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Center(child: Text('My Home Page!')),
+      color: Colors.red,
+    );
+  }
+}
+
+class Item1 extends StatelessWidget {
+  const Item1({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Center(child: Text('My Item1 Page!')),
+      color: Colors.green,
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Center(child: Text('My Item 2 Page!')),
+      color: Colors.blue,
+    );
+  }
+}
