a89170ac71e1c61b89ecad98f438e2bff6849ee2
Lalit Jarwal <64301870+lalitjarwal@users.noreply.github.com>
2020-09-22 12:55:29 +0530

    Create main.dart

---
+++ NasaOpenApi/main.dart
@@ -0,0 +1,18 @@
+import 'package:dartlang/nasaopenapi/ui.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Dart Lang';
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.blue),
+        title: title,
+        home:NasaHome());
+  }
+}
